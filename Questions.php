a1fe0b748f67d77122e6bfdb85853e7f44743228
Dawit-Tefera <himimengste1219@gmail.com>
2022-09-07 07:48:15 -0700

    my Ansewer

--- Questions.php
+++ Questions.php
@@ -3,5 +3,26 @@
 echo "Questions";
 echo "<br/>";
 echo "Write the steps to configure git <br/>";
+Step 1: Create a GitHub account
+Step 2: Create a new repository
+Step 3: Create a file
+Step 4: Make a commit
+To make a commit, enter:
+git commit -m "first commit"
+Step 5: Connect your GitHub repo with your computer
+Now, it's time to connect your computer to GitHub with the command:
+git remote add origin https://github.com/<your_username>/Demo.git
 echo "Write the difference and similarity of git and github <br/>";
+GIT
+	Git is a version control system that lets you manage and keep track of your source code history.
+	Git can be used completely exclusive of any cloud-hosting service — you don’t even need internet access, except to download it.
+	Git is installed and maintained on your local system (rather than in the cloud) and gives you a self-contained record of your ongoing programming versions. 
+	Git is also specially designed to work well with text files — which, if you think about it, is what code actually is. But one thing that really sets Git apart is its branching model.
+GITHUB
+	It’s an online database that allows you to keep track of and share your Git version control projects outside of your local computer/server. Unlike Git, GitHub is exclusively cloud-based. 
+	Also, unlike Git, GitHub is a for-profit service (although basic repository-hosting features are available at no cost to those who are willing to create a user profile, making GitHub a popular choice for open-source projects).
+	GitHub expands upon Git’s basic functionality.
+	Through GitHub, you can share your code with others, giving them the power to make revisions or edits on your various Git branches.
+	GitHub is a cloud-based hosting service that lets you manage Git repositories.
+
 ?>
